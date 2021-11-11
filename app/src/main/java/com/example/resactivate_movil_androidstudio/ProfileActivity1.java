d5d8e684ac40c746f356d5743901023afeb5fac1
mticclass16g2 <90979572+mticclass16g2@users.noreply.github.com>
2021-11-10 22:16:58 -0500

    "Login - Profile1 - Profile2 - 09-NOV-2021"

--- app/src/main/java/com/example/resactivate_movil_androidstudio/ProfileActivity1.java
+++ app/src/main/java/com/example/resactivate_movil_androidstudio/ProfileActivity1.java
@@ -2,7 +2,9 @@ package com.example.resactivate_movil_androidstudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfileActivity1 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class ProfileActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_1);
+    }
+    public void iniciarProfile2(View view){
+        Intent i = new Intent(this, ProfileActivity2.class);
+        startActivity(i);
     }
 }
